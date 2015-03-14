6b0282e2efd6b6e079aeb0aca832fbfbc1967b6b
Torben Knerr <mail@tknerr.de>
2015-03-14 02:52:12 +0100

    make it work for windows / winrm communicator too

--- lib/vagrant-managed-servers/action/warn_networks.rb
+++ lib/vagrant-managed-servers/action/warn_networks.rb
@@ -15,8 +15,9 @@ module VagrantPlugins
         end
 
         def custom_networking_defined?(env)
+          communicator = env[:machine].config.vm.communicator
           network_configs = env[:machine].config.vm.networks
-          custom_network_configs = (network_configs - default_network_configs)
+          custom_network_configs = (network_configs - default_network_configs(communicator))
           custom_network_configs.any?
         end
 
@@ -24,7 +25,23 @@ module VagrantPlugins
         # these are the port forwardings which are always created by default, see:
         # https://github.com/mitchellh/vagrant/blob/a8dcf92f14/plugins/kernel_v2/config/vm.rb#L393-417
         #
-        def default_network_configs
+        def default_network_configs(communicator)
+          winrm = [:forwarded_port, {
+            guest: 5985,
+            host: 55985,
+            host_ip: "127.0.0.1",
+            id: "winrm",
+            auto_correct: true,
+            protocol: "tcp"
+          }]
+          winrm_ssl = [:forwarded_port, {
+            guest: 5986,
+            host: 55986,
+            host_ip: "127.0.0.1",
+            id: "winrm-ssl",
+            auto_correct: true,
+            protocol: "tcp"
+          }]
           ssh = [:forwarded_port, {
             guest: 22,
             host: 2222,
@@ -33,7 +50,11 @@ module VagrantPlugins
             auto_correct: true,
             protocol: "tcp"
           }]
-          return [ ssh ]
+          if communicator == :winrm
+            [ winrm, winrm_ssl ]
+          else
+            [ ssh ]
+          end
         end
       end
     end
