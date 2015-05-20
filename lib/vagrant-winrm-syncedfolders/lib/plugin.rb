042d071a7d50127ad36dde2c03195d8c9fb0d960
Christopher Baldauf <cbaldauf@vistaprint.com>
2015-05-20 16:39:57 -0400

    WIP WinRM Synced folders plugin. Will move it to a new plugin repo once initial testing looks good.

---
+++ lib/vagrant-winrm-syncedfolders/lib/plugin.rb
@@ -0,0 +1,24 @@
+require "vagrant"
+
+module VagrantPlugins
+  module SyncedFolderWinRM
+    # This plugin implements WinRM synced folders.
+    class Plugin < Vagrant.plugin("2")
+      name "WinRM synced folders"
+      description <<-EOF
+      The WinRM synced folders plugin enables you to use WinRM as a mechanism
+      to transfer files to a guest machine. There are known limitations to this
+      mechanism, most notably that file tranfer is slow for large numbers of files.
+      EOF
+
+      synced_folder("wirnm", 6) do
+        require_relative "synced_folder"
+        init!
+        SyncedFolder
+      end
+
+      def self.init!
+      end
+    end
+  end
+end
